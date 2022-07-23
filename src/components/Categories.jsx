0955de83c9a3fb5a9bf3ee26f2026c4cbf3eea17
balabaev00 <balabaev00@yandex.ru>
2022-07-23 22:36:06 +0900

    add categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,26 @@
+import React, {useState} from 'react'
+
+
 
 function Categories() {
+
+	const [indexActiveCategory, setIndexActiveCategory] = useState(0);
+
+	const categories = [`Все`, `Мясные`, `Гриль`, `Острые`, `Без лука`]
+
+	const onClickCategory = (index) => {
+		setIndexActiveCategory(index)
+	}
+
 	return (
-		<div class="categories">
+		<div className="categories">
 			<ul>
-				<li class="active">Все</li>
-				<li>Мясные</li>
-				<li>Вегетарианская</li>
-				<li>Гриль</li>
-				<li>Острые</li>
-				<li>Закрытые</li>
+				{
+					categories.map((category, index) => (
+						<li onClick={() => onClickCategory(index)}  className={indexActiveCategory === index? 'active': ''}>{category}</li>
+					))
+				}
+				
 			</ul>
 		</div>
 	)
